a637f6729bbb0e1c1b1b687287492e2029a454aa
Damaro684 <danielmateos684@gmail.com>
2025-04-30 18:57:55 +0200

    Dia 2 - 1.2

--- js/main.js
+++ js/main.js
@@ -18,17 +18,17 @@ const personalizado = document.getElementsByClassName("personalizado");
 
 let intentos = 0;
 
-aceptar.addEventListener("click",() => {
+aceptar.addEventListener("click", () => {
 
-    if(nombre.value===""){
+    if (nombre.value === "") {
         alert("Introduce un nombre.");
         return;
     }
 
-    if(tema.value==="aleatorio"){
+    if (tema.value === "aleatorio") {
         const temas = document.getElementsByClassName("temas");
 
-        let numRnd = Math.floor(4*Math.random());
+        let numRnd = Math.floor(4 * Math.random());
 
         tema.value = temas[numRnd].value;
     }
@@ -38,68 +38,93 @@ aceptar.addEventListener("click",() => {
     poneNombre();
     poneTema();
 
-    colocaImagenes();
+    if (!tomaValoresFilas()) {
+        return;
+    }
+
+    colocaTarjetas();
 
     inicio.style.display = "none";
     juego.style.display = "block";
 })
 
 
-nivel.addEventListener("click",() => {
-    if(nivel.value==="personalizado"){
+nivel.addEventListener("click", () => {
+    if (nivel.value === "personalizado") {
 
-        for(const tr of personalizado){
+        for (const tr of personalizado) {
             tr.style.display = "table-row"
         }
 
     }
 
-    if(nivel.value!=="personalizado"){
+    if (nivel.value !== "personalizado") {
 
-        for(const tr of personalizado){
+        for (const tr of personalizado) {
             tr.style.display = "none"
         }
 
     }
 })
 
-function actIntentos(){
-    intent.textContent = "Intentos: " +  intentos;
+function actIntentos() {
+    intent.textContent = "Intentos: " + intentos;
 }
 
-function poneNombre(){
-    hNombre.textContent = "Jugador: " +  nombre.value;
+function poneNombre() {
+    hNombre.textContent = "Jugador: " + nombre.value;
 }
 
-function poneTema(){
-    hTema.textContent = "Tema: " +  tema.value;
+function poneTema() {
+    hTema.textContent = "Tema: " + tema.value;
 }
 
 let nFilas;
 let nColumnas;
 
-const persFilas = document.getElementById
-const persFilas = document.getElementById
+const persFilas = document.getElementById("persFilas");
+const persColumnas = document.getElementById("persColumnas");
 
 
-function colocaImagenes(){
-    switch (nivel.value){
+function tomaValoresFilas() {
+    switch (nivel.value) {
         case "facil":
             nFilas = 4;
             nColumnas = 4;
-        break;
+            break;
         case "medio":
             nFilas = 5;
             nColumnas = 6;
-        break;
+            break;
         case "dificil":
             nFilas = 6;
             nColumnas = 6;
-        break;
+            break;
         case "personalizado":
+            nFilas = persFilas.value;
+            nColumnas = persColumnas.value;
 
-        break;
+            if ((nFilas * nColumnas) % 2 === 1) {
+                alert("El número total de tarjetas debe ser par. Cambia los valores personalizados.");
+                return false;
+            }
+            break;
     }
+    return true;
+
+}
+
+const wrapper = document.getElementById("wrapper");
+
+function colocaTarjetas() {
+    for (var i = 0; i < nFilas * nColumnas; i++) {
+        const carta = document.createElement("div");
+        carta.classList.add("carta");
+
+        carta.innerHTML = "Hola";
+
+        wrapper.appendChild(carta);
 
 
+    }
 }

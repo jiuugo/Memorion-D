9b9fec09a3f50b2406c3eb617b00810bc12118a0
Damaro684 <danielmateos684@gmail.com>
2025-04-30 17:03:07 +0200

    Dia 2 - 1.0

--- js/main.js
+++ js/main.js
@@ -2,28 +2,79 @@ const inicio = document.getElementById("inicio");
 const juego = document.getElementById("juego");
 
 const nombre = document.getElementById("nombre");
+const modo = document.getElementById("modo");
+const nivel = document.getElementById("nivel");
+const tema = document.getElementById("tema");
+const temp = document.getElementById("temp");
 
 
 const aceptar = document.getElementById("btnJugar");
 
 const hNombre = document.getElementById("hNombre");
-
+const hTema = document.getElementById("hTema");
 const intent = document.getElementById("intent");
+
+const personalizado = document.getElementsByClassName("personalizado");
 
 let intentos = 0;
 
 aceptar.addEventListener("click",() => {
-    inicio.style.display = "none";
-    juego.style.display = "block";
+
+    if(nombre.value===""){
+        alert("Introduce un nombre.");
+        return;
+    }
+
+    if(tema.value==="aleatorio"){
+        const temas = document.getElementsByClassName("temas");
+
+        let numRnd = Math.floor(4*Math.random());
+
+        tema.value = temas[numRnd].value;
+    }
+
+    alert(tema.value);
 
     actIntentos();
     poneNombre();
+    poneTema();
+
+    inicio.style.display = "none";
+    juego.style.display = "block";
 })
 
+
+nivel.addEventListener("click",() => {
+    if(nivel.value==="personalizado"){
+
+        for(const tr of personalizado){
+            tr.style.display = "table-row"
+        }
+
+    }
+
+    if(nivel.value!=="personalizado"){
+
+        for(const tr of personalizado){
+            tr.style.display = "none"
+        }
+
+    }
+})
+
+
+
+
+
+
 function actIntentos(){
-    intent.textContent = "Intentos: "+  intentos;
+    intent.textContent = "Intentos: " +  intentos;
 }
 
 function poneNombre(){
-    hNombre.textContent = "Jugador: "+  nombre.value;
+    hNombre.textContent = "Jugador: " +  nombre.value;
+}
+
+function poneTema(){
+    hTema.textContent = "Tema: " +  tema.value;
 }
